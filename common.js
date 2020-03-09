3f6326c52df74aef3488363a26d0f90bf22ce42a
szrudi <mail@szaborudi.hu>
2020-03-09 09:05:23 +0000

    added other Promise features

--- common.js
+++ common.js
@@ -1,9 +1,10 @@
-function print(text) {
-    const output = document.getElementById('outputText');
+function printText(text) {
+    const output = document.querySelector('#outputText');
     output.value += text + '\n';
+    return text;
 }
 
 function clear() {
-    const output = document.getElementById('outputText');
+    const output = document.querySelector('#outputText');
     output.value = '';
 }
--- script.js
+++ script.js
@@ -1,70 +1,101 @@
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises
-// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Promises
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-// https://pouchdb.com/2015/05/18/we-have-a-problem-with-promises.html
+/*
+Background articles:
+ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises
+ https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Promises
+ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+ https://pouchdb.com/2015/05/18/we-have-a-problem-with-promises.html
+*/
 
 /*
-You should have:
+The task is to wake up Neo and for this we should writes thes on his console:
 (in any order)
-hey!
-wake up!
-hey you! wake up!
-(only after all those function finish, the final function is called)
-Wake up Neo
+ - hey!
+ - wake up!
+ - Wake up, Neo... (for this we need to figure out the name)
+ - hey you! wake up!
+
+Only after all those function finish, the final message:
+ - The Matrix has you...
+
+And then the command for Neo:
+(but we have to figure out the color of the rabbit):
+ - Follow the white rabbit. 
 */
-clear();
-let promises = [];
-promises.push(delayMessage(nudgeWakeUp));
-promises.push(delayMessage(nudgeHey));
-promises.push(delayMessage(nudgeHeyWakeUp));
 
-Promise.all(promises)
-    .then(whiteRabbit)
-    .catch(() => console.log('could not wake him up!'));
+wakeUpNeo()
+    .then(heHasAwaken)
+    .then(() => getRabbitColor(followTheRabbit))
+    .catch((error) => console.log('Could not wake him up!', error));
 
-function delayMessage(nudge) {
-    return new Promise(resolvePromise => {
-        setTimeout(() => {
-                const msg = nudge();
-                print(msg);
-                resolvePromise(msg);
-            },
-            500 + Math.random() * 5000
-        );
-    });
+function wakeUpNeo() {
+    clear();
+    let promises = [];
+    promises.push(nudgeWakeUp());
+    promises.push(nudgeHey());
+    promises.push(nudgeHeyWakeUp());
+    promises.push(getHckrName(nudgeByName));
+    return Promise.all(promises);
 }
 
 function nudgeWakeUp() {
-    return 'wake up!';
+    return createPromise('wake up!').then(printText);
 }
 
 function nudgeHeyWakeUp() {
-    return 'hey you! wake up!';
+    return createPromise('hey you! wake up!').then(printText);
 }
 
 function nudgeHey() {
-    return 'hey!';
+    return createPromise('hey!').then(printText);
 }
 
-function whiteRabbit(previousMessages) {
-    setTimeout(
-        () => {
+function getHckrName(nudgeHacker) {
+    return createPromise(Math.floor(Math.random() * 4) ? 'Neo' : 'HackerMan')
+        .then((name) => {
+            console.log('Hacker name:', name);
+            return nudgeHacker(name);
+        });
+}
+
+function getRabbitColor(rabbitCommand) {
+    return createPromise('white').then((color) => {
+        console.log('Rabbit color:', color);
+        rabbitCommand(color);
+    });
+}
+
+function nudgeByName(name) {
+    if (name !== "Neo") {
+        return createPromise('Canceling wakeup...\nGo to sleep ' + name)
+        .then((msg) => {
             clear();
-            print('Wake up, Neo...');
-            print('The Matrix has you.');
-            print('');
-            print(`We've sent you ${previousMessages.length} messages before.`);
-            console.log('Previous message that did not wake him up: ', previousMessages);
-        },
-        1500);
+            printText(msg);
+            throw 'Unknown Hacker: ' + name;
+        })
+    } else {
+        return createPromise(`Wake up, ${name}...`).then(printText);
+    }
+
 }
 
-function print(text) {
-    const output = document.getElementById('outputText');
-    output.value += text + '\n';
+function heHasAwaken(previousMessages) {
+    return createPromise('The Matrix has you...')
+        .then((msg) => {
+            clear();
+            console.log('Previous messages that we sent before: ', previousMessages);
+            return printText(msg);
+        });
 }
 
-function clear() {
-    const output = document.getElementById('outputText');
-    output.value = '';
+function followTheRabbit(color) {
+    return createPromise(`Follow the ${color} rabbit.`).then(printText);
 }
+
+function createPromise(message) {
+    return new Promise((resolvePromise, rejectPromise) => {
+        console.log(`starting promise "${message}"`);
+        let delay = 1000 + Math.random() * 2000
+        setTimeout(() => resolvePromise(message), delay);
+    });
+}
+
